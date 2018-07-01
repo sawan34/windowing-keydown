facfecfe4179ecf8aadcba286be9f6aee54f4fd9
sawan <sawankumar3jan@gmail.com>
2018-07-02 04:05:30 +0530

    keydown

---
+++ demo/src/index.js
@@ -0,0 +1,124 @@
+import React, { PureComponent } from "react"
+import { render } from "react-dom"
+
+import VirtualSlider from "../../src"
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function randomColor() {
+  const red = getRandomIntInclusive(0, 255)
+  const green = getRandomIntInclusive(0, 255)
+  const blue = getRandomIntInclusive(0, 255)
+
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+const styles = {
+  item: {
+    width: 151,
+    height: 188,
+    display: "inline-block"
+  },
+  container: {
+    overflow: "hidden"
+  }
+}
+
+class Demo extends PureComponent {
+
+  constructor(){
+    super();
+    this.indexData=0;
+    this.handleKeyEvent =  this.handleKeyEvent.bind(this)
+  }
+
+  renderItem = (index, key) => {
+    return (
+      <div key={key} style={{ ...styles.item, backgroundColor: randomColor() }}>
+        {index}
+      </div>
+    )
+  }
+
+
+  componentDidMount(){
+    document.addEventListener('keydown',this.handleKeyEvent)
+  }
+
+ 
+
+  handleKeyEvent(e){
+    const keyCode =  e.keyCode;
+    console.log("key Code: "+e.keyCode);
+    if(keyCode ===37){
+      if(this.indexData >0){
+        this.indexData =  this.indexData - 1;
+        this.slider.scrollTo(this.indexData)
+
+      }
+    }
+
+    if(keyCode===39){
+      if(this.indexData < 30){
+        this.indexData =  this.indexData + 1;
+        this.slider.scrollTo(this.indexData);
+      }
+    }
+  
+    
+  }
+
+  handlePrev = () => {
+    this.slider.goBack()
+  }
+
+  handleNext = () => {
+    this.slider.goNext()
+  }
+
+  goToFirst = () => {
+    this.slider.scrollTo(3)
+  }
+
+  goto = position => {
+    this.slider.scrollTo(position)
+  }
+
+  handleStart = () => {
+    console.log("start")
+  }
+
+  handleEnd = () => {
+    console.log("end")
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>react-virtual-slider Demo</h1>
+        <h1>TOTAL: 50,000 items</h1>
+        <VirtualSlider
+          ref={slider => (this.slider = slider)}
+          itemRenderer={this.renderItem}
+          itemSize={151}
+          length={30}
+          containerStyle={styles.container}
+          startAction={this.handleStart}
+          endAction={this.handleEnd}
+        />
+
+        <button onClick={this.handlePrev}>Prev</button>
+        <button onClick={this.handleNext}>Next</button>
+        <button onClick={this.goToFirst}>FIRST</button>
+        <button onClick={this.goto.bind(null, 500)}>500th item</button>
+      </div>
+    )
+  }
+}
+
+render(<Demo />, document.querySelector("#demo"))
---
+++ src/index.js
@@ -0,0 +1,150 @@
+import React, { PureComponent } from 'react'
+import { findDOMNode } from 'react-dom'
+import { getSizes, getItemPerPage, getAnimatingOffset } from './utils'
+
+class VirtualList extends PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      itemPerPage: 0,
+      from: 0,
+      cursor: 0,
+      viewportWidth: 0,
+      
+    }
+    this.key= {};
+  }
+
+  scrollTo = index => {
+    const { startAction, endAction, length}  = this.props
+    const { itemPerPage } = this.state
+
+    if (index === 0) {
+      startAction && startAction()
+    }
+
+    if (index === length - itemPerPage) {
+      endAction && endAction()
+    }
+
+    if (index === this.state.from) return
+
+    this.setState({
+      from: index
+    })
+    this.list.removeEventListener('transitionend', this.lastTransitionendAction)
+    this.lastTransitionendAction = null
+  }
+
+  componentDidMount() {
+    this.container = findDOMNode(this)
+    this.calculateItemPerPage(this.props, this.container)
+
+    window.addEventListener(
+      'resize',
+      this.calculateItemPerPage.bind(null, this.props, this.container)
+    )
+
+    document.addEventListener('keydown',this.handleKeyEvent)
+
+  }
+
+  handleKeyEvent(e){
+    this.key = e;
+  }
+  componentWillUnmount() {
+    window.removeEventListener(
+      'resize',
+      this.calculateItemPerPage.bind(null, this.props, this.container)
+    )
+  }
+
+  calculateItemPerPage = (props, container) => {
+    const { itemSize } = props
+    const viewportWidth = container.offsetWidth
+    const itemPerPage = getItemPerPage(viewportWidth, itemSize)
+
+    this.setState({
+      itemPerPage,
+      viewportWidth
+    })
+  }
+
+  goBack = () => {
+    const { length, itemSize } = this.props
+    const { itemPerPage, from, viewportWidth } = this.state
+    const prevValue = Math.max(from - itemPerPage, 0)
+    const nextStyle = getSizes(itemSize, itemPerPage, prevValue, length, viewportWidth)
+
+    this.list.style.transform = `translateX(0px)`
+    this.list.style.transition = 'transform 0.5s ease'
+    const doSetState = () => {
+      this.list.style.transition = 'none'
+      this.list.style.transform = `translateX(-${nextStyle.offsetLeft}px)`
+      this.scrollTo(prevValue)
+    }
+
+    if (this.lastTransitionendAction) {
+      this.list.removeEventListener("transitionend", this.lastTransitionendAction)
+    }
+    this.list.addEventListener('transitionend', doSetState)
+    this.lastTransitionendAction = doSetState
+  }
+
+  goNext = () => {
+    const { length, itemSize } = this.props
+    const { itemPerPage, from, viewportWidth } = this.state
+    const nextStart = Math.min(from + itemPerPage, length - itemPerPage)
+
+    const nextStyle = getSizes(itemSize, itemPerPage, nextStart, length, viewportWidth)
+    const animatingOffset = getAnimatingOffset(itemSize, itemPerPage, from, length, viewportWidth, nextStart)
+
+    this.list.style.transform = `translateX(-${animatingOffset}px)`
+    this.list.style.transition = 'transform 0.5s ease'
+    const doSetState = () => {
+      this.list.style.transition = 'none'
+      this.list.style.transform = `translateX(-${nextStyle.offsetLeft}px)`
+      this.scrollTo(nextStart)
+    }
+
+    if (this.lastTransitionendAction) {
+      this.list.removeEventListener("transitionend", this.lastTransitionendAction)
+    }
+    this.list.addEventListener('transitionend', doSetState)
+    this.lastTransitionendAction = doSetState
+  }
+
+  render() {
+    const { length, itemSize, itemRenderer, containerStyle } = this.props
+    const { itemPerPage, from, viewportWidth } = this.state
+
+    const { end, offsetLeft, start, visibleStartIndex } = getSizes(
+      itemSize,
+      itemPerPage,
+      from,
+      length,
+      viewportWidth
+    )
+
+    const style = {
+      width: itemSize * length,
+      transform: `translateX(-${offsetLeft}px)`,
+      willChange: 'transform'
+    }
+
+    let items = []
+    for (let i = start; i < end; i++) {
+      items.push(itemRenderer(i, i))
+    }
+
+    return (
+      <div className="slider-container" style={containerStyle}>
+        <div style={style} ref={node => (this.list = node)}>
+          {items}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default VirtualList
